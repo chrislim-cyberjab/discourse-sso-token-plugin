e11a1ccea52b7d24c5ac84c37692538c7141603e
chrislim-cyberjab <chris.lim@cyberjab.org>
2026-03-30 13:17:57 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,3 @@ DiscourseSSOToken::Engine.routes.draw do
   get "/capture" => "sso_token#capture_token"
 end
 
-Discourse::Application.routes.append do
-  mount ::DiscourseSSOToken::Engine, at: "/sso-token"
-end
